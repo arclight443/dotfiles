7e5ba80e8a118e5be70da31756f9a20f330fe296
Jump <waratch.p@outlook.com>
2023-11-11 09:09:40 +0700

    feat: add edgy.nvim and adjust keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,6 +1,7 @@
 -- Default keymaps that are always set:
 -- https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 
+local Util = require("lazyvim.util")
 local keymap = vim.keymap.set
 local telescope = require("telescope.builtin")
 
@@ -18,8 +19,38 @@ end, desc("Delete Buffer"))
 keymap("n", "<leader>sx", "<cmd>Telescope resume<CR>", desc("Resume Telescope"))
 keymap("n", "<leader>fz", "<cmd>Telescope notify<CR>", desc("Notification log"))
 
+--LazyTerm
+vim.keymap.del("t", "<C-h>")
+vim.keymap.del("t", "<C-j>")
+vim.keymap.del("t", "<C-k>")
+vim.keymap.del("t", "<C-l>")
+vim.keymap.del("n", "<C-/>")
+vim.keymap.del("t", "<C-/>")
+
+local lazyterm = function()
+  Util.terminal(nil, { cwd = Util.root() })
+end
+
+keymap("n", "<M-\\>", lazyterm, { desc = "Terminal (root dir)" })
+keymap("t", "<M-\\>", "<cmd>close<cr>", { desc = "Hide Terminal" })
+
+keymap("t", "<M-h>", "<cmd>wincmd h<cr>", { desc = "Go to left window" })
+keymap("t", "<M-j>", "<cmd>wincmd j<cr>", { desc = "Go to lower window" })
+keymap("t", "<M-k>", "<cmd>wincmd k<cr>", { desc = "Go to upper window" })
+keymap("t", "<M-l>", "<cmd>wincmd l<cr>", { desc = "Go to right window" })
+
+-- Window resize
+vim.keymap.del("n", "<C-up>")
+vim.keymap.del("n", "<C-down>")
+vim.keymap.del("n", "<C-left>")
+vim.keymap.del("n", "<C-right>")
+
 -- Tmux
-keymap("n", "<M-h>", "<cmd>NvimTmuxNavigateLeft<CR>")
-keymap("n", "<M-j>", "<cmd>NvimTmuxNavigateDown<CR>")
-keymap("n", "<M-k>", "<cmd>NvimTmuxNavigateUp<CR>")
-keymap("n", "<M-l>", "<cmd>NvimTmuxNavigateRight<CR>")
+vim.keymap.del("n", "<C-h>")
+vim.keymap.del("n", "<C-j>")
+vim.keymap.del("n", "<C-k>")
+vim.keymap.del("n", "<C-l>")
+keymap("n", "<M-h>", "<cmd>NvimTmuxNavigateLeft<CR>", desc("Go to left window"))
+keymap("n", "<M-j>", "<cmd>NvimTmuxNavigateDown<CR>", desc("Go to lower window"))
+keymap("n", "<M-k>", "<cmd>NvimTmuxNavigateUp<CR>", desc("Go to upper window"))
+keymap("n", "<M-l>", "<cmd>NvimTmuxNavigateRight<CR>", desc("Go to right window"))
